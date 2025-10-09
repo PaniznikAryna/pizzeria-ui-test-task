f46917cf766e31a7d00275186e65a45408f77db1
Paniznik Aryna <arinapaniznik4@gmail.com>
2025-10-09 17:24:46 +0300

    update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,5 @@
 package org.example;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-
 public class Main {
     public static void main(String[] args) {
 //        ChromeOptions chromeOptions = new ChromeOptions();
