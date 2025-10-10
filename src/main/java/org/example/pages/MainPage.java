b4f0bc83ac4bc3f22e548fd5a080a1412a002d26
Paniznik Aryna <arinapaniznik4@gmail.com>
2025-10-10 16:55:17 +0300

    wrote tests for the Main page

--- src/main/java/org/example/pages/MainPage.java
+++ src/main/java/org/example/pages/MainPage.java
@@ -1,4 +1,118 @@
 package org.example.pages;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class MainPage {
+    private WebDriver driver;
+
+    public MainPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//a[@aria-label=\"next\"]")
+    private WebElement rightNavigationButton;
+
+    public void clickRightSlider() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", rightNavigationButton);
+    }
+
+
+    public boolean isRightArrowVisible() {
+        return rightNavigationButton.isDisplayed();
+    }
+
+    public WebElement getRightArrow() {
+        return rightNavigationButton;
+    }
+
+    @FindBy(xpath = "//a[@aria-label=\"previous\"]")
+    private WebElement leftNavigationButton;
+
+    public void clickLeftSlider(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", leftNavigationButton);
+    }
+
+
+    public boolean isLeftArrowVisible() {
+        return leftNavigationButton.isDisplayed();
+    }
+
+    public WebElement getLeftArrow() {
+        return leftNavigationButton;
+    }
+
+    @FindBy(xpath = "//*[contains(@class, 'slick-slide') and contains(@class, 'slick-active')]")
+    private WebElement activeSlide;
+
+    public String getActiveSlideIndex() {
+        return activeSlide.getAttribute("data-slick-index");
+    }
+
+    @FindBy(xpath = "//a[@data-product_id=\"431\"]")
+    private WebElement hoverButtonInBasket;
+
+    public boolean displayButtonInBasket(){
+        return hoverButtonInBasket.isDisplayed();
+    }
+
+    public WebElement getButtonInBasketElement() {
+        return hoverButtonInBasket;
+    }
+
+
+    @FindBy(xpath = "//img[@src=\"http://pizzeria.skillbox.cc/wp-content/uploads/2021/10/pexels-chevanon-photography-312418-300x300.jpg\"]")
+    private WebElement drinkSliderElement;
+
+    public void hoverDrinkSlider() {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", drinkSliderElement);
+        Actions action = new Actions(driver);
+        action.moveToElement(drinkSliderElement).perform();
+    }
+
+    @FindBy(xpath = "//img[@src=\"http://pizzeria.skillbox.cc/wp-content/uploads/2021/10/pexels-geraud-pfeiffer-6607296-300x300.jpg\"]")
+    private WebElement desertSlideElement;
+
+    public void clickDessertImg(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", desertSlideElement);
+    }
+
+    @FindBy(xpath = "//a[@title=\"Десерт «Булочка с корицей»\"]")
+    private WebElement linkDesert;
+
+    public String getDesertLink(){
+      return linkDesert.getAttribute("href");
+    }
+
+    @FindBy(xpath = "//div[@id=\"ak-top\"]")
+    private WebElement upArrowLink;
+
+    public boolean displayupArrowLink(){
+        return upArrowLink.isDisplayed();
+    }
+
+    public void scrollPage(){
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+    }
+
+    @FindBy(xpath = "//a[@rel=\"noopener noreferrer\"]")
+    private WebElement socialMediaLinks;
+
+    public void clickSocialMediaLinks(){
+        socialMediaLinks.click();
+    }
+
+    public String getSocialMediaLinks(){
+        return socialMediaLinks.getAttribute("href");
+    }
+
 }
--- src/test/java/org/example/test_task_4/MainPageTest.java
+++ src/test/java/org/example/test_task_4/MainPageTest.java
@@ -1,55 +1,118 @@
 package org.example.test_task_4;
 
+import org.example.pages.MainPage;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class MainPageTest {
-    //- Переключение пицц в слайдере (стрелки влево/вправо) +--
-    //- Наведение на картинку напитка с проверкой отображения ссылки «В корзину» +--
-    //- Переход на страницу десерта при клике по его картинке +--
-    //- Отображение ссылки-стрелочки «Наверх» в правом нижнем углу сайта при скроллинге в самый низ сайта +--
-    //- Открытие ссылок на социальные сети из футера страницы в новой вкладке +--
+    //- Переключение пицц в слайдере (стрелки влево/вправо) +++
+    //- Наведение на картинку напитка с проверкой отображения ссылки «В корзину» +++
+    //- Переход на страницу десерта при клике по его картинке ++-
+    //- Отображение ссылки-стрелочки «Наверх» в правом нижнем углу сайта при скроллинге в самый низ сайта ++-
+    //- Открытие ссылок на социальные сети из футера страницы в новой вкладке ++-
 
     WebDriver driver;
+    MainPage mainPage;
 
     @BeforeEach
    void setUp(){
         ChromeOptions chromeOptions = new ChromeOptions();
         driver = new ChromeDriver(chromeOptions);
         driver.get("https://pizzeria.skillbox.cc/");
+        mainPage = new MainPage(driver);
     }
 
     @Test
     void testRightNavigationSliderButton(){
 
+            String before = mainPage.getActiveSlideIndex();
+
+            mainPage.clickRightSlider();
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            wait.until(driver -> {
+                String current = mainPage.getActiveSlideIndex();
+                return !current.equals(before);
+            });
+
+            String after = mainPage.getActiveSlideIndex();
+            Assertions.assertNotEquals(before, after, "Слайдер не переключился");
     }
 
     @Test
     void testLeftNavigationSliderButton(){
 
+        String before = mainPage.getActiveSlideIndex();
+
+        mainPage.clickLeftSlider();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(driver -> {
+            String current = mainPage.getActiveSlideIndex();
+            return !current.equals(before);
+        });
+
+        String after = mainPage.getActiveSlideIndex();
+        Assertions.assertNotEquals(before, after, "Слайдер не переключился");
     }
 
     @Test
-    void testDisplayingLinkToTheBasket(){
+    void testDisplayingLinkToTheBasket() {
+        mainPage.hoverDrinkSlider();
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(mainPage.getButtonInBasketElement()));
+
+        Assertions.assertTrue(mainPage.displayButtonInBasket(), "Кнопка для перехода 'В корзину' не появилась при наведении на напиток");
     }
+
+
 
     @Test
     void testGoToTheDessertPage(){
 
+        String expectedUrl = mainPage.getDesertLink();
+        mainPage.clickDessertImg();
+        String actualUrl = driver.getCurrentUrl();
+        Assertions.assertEquals(expectedUrl, actualUrl, "URL страницы не соответствует ожидаемому");
     }
 
     @Test
     void testDisplayingTheUpArrowLink(){
-
+        mainPage.scrollPage();
+        Assertions.assertTrue(mainPage.displayupArrowLink(), "Отсутствует отображение ссылки-стрелочки «Наверх»");
     }
 
     @Test
-    void testOpeningSocialMediaLinksInNewTab(){
+    void testOpeningSocialMediaLinksInNewTab() {
+        String expectedUrl = mainPage.getSocialMediaLinks();
+        mainPage.clickSocialMediaLinks();
 
+        String originalWindow = driver.getWindowHandle();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(driver -> driver.getWindowHandles().size() > 1);
+
+        for (String windowHandle : driver.getWindowHandles()) {
+            if (!windowHandle.equals(originalWindow)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+
+        String actualUrl = driver.getCurrentUrl();
+        Assertions.assertEquals(expectedUrl, actualUrl, "Страница перехода не соответствует ожидаемой");
+
+        driver.close();
+        driver.switchTo().window(originalWindow);
     }
+
 
     @AfterEach
     void tearDown() {
