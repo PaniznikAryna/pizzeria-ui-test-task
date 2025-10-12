59ffe109a87aa0640ad06b942598e7d01a95f2d1
Paniznik Aryna <arinapaniznik4@gmail.com>
2025-10-12 23:18:50 +0300

    wrote tests for the Navigation menu

--- src/main/java/org/example/pages/NavigationMenu.java
+++ src/main/java/org/example/pages/NavigationMenu.java
@@ -1,4 +1,62 @@
 package org.example.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class NavigationMenu {
+    private WebDriver driver;
+
+    public NavigationMenu(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(xpath = "//li[@id=\"menu-item-389\"]")
+    private WebElement menuButton;
+
+    public void moveToMenuButton(){
+        Actions action = new Actions(driver);
+        action.moveToElement(menuButton).perform();
+    }
+
+    @FindBy(xpath = "//li[@id=\"menu-item-390\"]")
+    private WebElement pizzaInMenuButton;
+
+    public void clickPizzaInMenuButton(){
+        pizzaInMenuButton.click();
+    }
+
+    public String getPizzaMenuLink() {
+        WebElement link = pizzaInMenuButton.findElement(By.tagName("a"));
+        return link.getAttribute("href");
+    }
+
+    @FindBy(xpath = "//li[@id=\"menu-item-391\"]")
+    private WebElement desertInMenuButton;
+
+    public void clickDesertInMenuButton(){
+        desertInMenuButton.click();
+    }
+
+    public String getDesertMenuLink() {
+        WebElement link = desertInMenuButton.findElement(By.tagName("a"));
+        return link.getAttribute("href");
+    }
+
+    @FindBy(xpath = "//li[@id=\"menu-item-393\"]")
+    private WebElement drinkInMenuButton;
+
+    public void clickDrinkInMenuButton(){
+        drinkInMenuButton.click();
+    }
+
+    public String getDrinkMenuLink() {
+        WebElement link = drinkInMenuButton.findElement(By.tagName("a"));
+        return link.getAttribute("href");
+    }
+
 }
--- src/test/java/org/example/test_task_4/NavigationMenuTest.java
+++ src/test/java/org/example/test_task_4/NavigationMenuTest.java
@@ -1,37 +1,53 @@
 package org.example.test_task_4;
 
+import org.example.pages.NavigationMenu;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class NavigationMenuTest {
-    //- Переход по всем разделам:
-    //  - Меню -> Пицца +--
-    //  - Меню -> Десерты +--
-    //  - Меню -> Напитки +--
 
     WebDriver driver;
+    NavigationMenu navigationMenu;
 
     @BeforeEach
     void setUp(){
         ChromeOptions chromeOptions = new ChromeOptions();
         driver = new ChromeDriver(chromeOptions);
         driver.get("https://pizzeria.skillbox.cc/");
+        navigationMenu = new NavigationMenu(driver);
     }
 
     @Test
     void testSwitchingFromMenuToPizza(){
+        navigationMenu.moveToMenuButton();
+        navigationMenu.clickPizzaInMenuButton();
+
+        String expectedUrl = navigationMenu.getPizzaMenuLink().replace("https://", "").replace("http://", "");
+        String actualUrl = driver.getCurrentUrl().replace("https://", "").replace("http://", "");
+        Assertions.assertEquals(expectedUrl, actualUrl, "URL страницы не соответствует ожидаемому");
 
     }
 
     @Test
     void testSwitchingFromMenuToDesserts(){
+        navigationMenu.moveToMenuButton();
+        navigationMenu.clickDesertInMenuButton();
 
+        String expectedUrl = navigationMenu.getDesertMenuLink().replace("https://", "").replace("http://", "");
+        String actualUrl = driver.getCurrentUrl().replace("https://", "").replace("http://", "");
+        Assertions.assertEquals(expectedUrl, actualUrl, "URL страницы не соответствует ожидаемому");
     }
 
     @Test
     void testSwitchingFromMenuToDrinks(){
+        navigationMenu.moveToMenuButton();
+        navigationMenu.clickDrinkInMenuButton();
+
+        String expectedUrl = navigationMenu.getDrinkMenuLink().replace("https://", "").replace("http://", "");
+        String actualUrl = driver.getCurrentUrl().replace("https://", "").replace("http://", "");
+        Assertions.assertEquals(expectedUrl, actualUrl, "URL страницы не соответствует ожидаемому");
 
     }
 
