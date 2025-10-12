8d381359b33ed28268996f8b51947ba8102269b1
Paniznik Aryna <arinapaniznik4@gmail.com>
2025-10-13 00:50:37 +0300

    wrote tests for the Delivery and Payment

--- src/main/java/org/example/pages/DeliveryAndPayment.java
+++ src/main/java/org/example/pages/DeliveryAndPayment.java
@@ -1,4 +1,40 @@
 package org.example.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class DeliveryAndPayment {
+    private WebDriver driver;
+
+    public DeliveryAndPayment(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//iframe")
+    private WebElement iframeDeliveryAndPayment;
+
+    public void switchToIframeDeliveryAndPayment() {
+        driver.switchTo().frame(iframeDeliveryAndPayment);
+    }
+
+    @FindBy(xpath = "//li[contains(.,'Минимальная сумма заказа')]")
+    private WebElement minOrderAmount;
+
+    public boolean minOrderAmountIs800Rubles() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(minOrderAmount));
+
+        return minOrderAmount.getText().contains("800 рублей");
+    }
+
+
+
+
 }
--- src/test/java/org/example/test_task_4/DeliveryAndPaymentTest.java
+++ src/test/java/org/example/test_task_4/DeliveryAndPaymentTest.java
@@ -1,26 +1,28 @@
 package org.example.test_task_4;
 
+import org.example.pages.DeliveryAndPayment;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DeliveryAndPaymentTest {
-    //- Проверка минимальной суммы заказа (800 руб) +--
 
     WebDriver driver;
+    DeliveryAndPayment deliveryAndPayment;
 
     @BeforeEach
     void setUp(){
         ChromeOptions chromeOptions = new ChromeOptions();
         driver = new ChromeDriver(chromeOptions);
         driver.get("https://pizzeria.skillbox.cc/delivery/");
+        deliveryAndPayment = new DeliveryAndPayment(driver);
     }
 
     @Test
     void testMinimumOrderAmount(){
-
-    }
+        deliveryAndPayment.switchToIframeDeliveryAndPayment();
+        Assertions.assertTrue(deliveryAndPayment.minOrderAmountIs800Rubles(), "Текст не содержит '800 рублей'");    }
 
     @AfterEach
     void tearDown(){
