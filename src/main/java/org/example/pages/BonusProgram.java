9de22132af2d299f768cea7f916d2985bac15020
Paniznik Aryna <arinapaniznik4@gmail.com>
2025-10-13 00:12:45 +0300

    wrote tests for the Bonus Program

--- src/main/java/org/example/pages/BonusProgram.java
+++ src/main/java/org/example/pages/BonusProgram.java
@@ -1,4 +1,56 @@
 package org.example.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BonusProgram {
+    private WebDriver driver;
+
+    public BonusProgram(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//input[@id=\"bonus_username\"]")
+    private WebElement bonusUserName;
+
+    public void FillingUserName(){
+        bonusUserName.sendKeys("Aryna");
+    }
+
+    @FindBy(xpath = "//input[@id=\"bonus_phone\"]")
+    private WebElement bonusPhone;
+
+    public void FillingPhone(){
+        bonusPhone.sendKeys("+79991231231");
+    }
+
+    @FindBy(xpath = "//button[@class=\"woocommerce-Button woocommerce-button button woocommerce-form-register__submit\"]")
+    private WebElement buttonIssueCard;
+
+    public void clickButtonIssueCard(){
+        buttonIssueCard.click();
+    }
+
+    public void closeAlert() {
+        driver.switchTo().alert().accept();
+    }
+
+    @FindBy(xpath = "//h3[starts-with(text(),'Ваша карта оформлена!')]")
+    private WebElement  cardHasBeenIssued;
+
+    public boolean displayH3WithCardHasBeenIssued(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOf(cardHasBeenIssued));
+        return cardHasBeenIssued.isDisplayed();
+    }
+
+
+
 }
--- src/test/java/org/example/test_task_4/BonusProgramTest.java
+++ src/test/java/org/example/test_task_4/BonusProgramTest.java
@@ -1,27 +1,38 @@
 package org.example.test_task_4;
 
+import org.example.pages.BonusProgram;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+
 public class BonusProgramTest {
-    //- Успешное оформление карты +--
 
     WebDriver driver;
+    BonusProgram bonusProgram;
+
 
     @BeforeEach
     void setUp(){
         ChromeOptions chromeOptions = new ChromeOptions();
         driver = new ChromeDriver(chromeOptions);
         driver.get("https://pizzeria.skillbox.cc/bonus/");
+        bonusProgram = new BonusProgram(driver);
     }
 
     @Test
     void testMakingCard(){
+        bonusProgram.FillingUserName();
+        bonusProgram.FillingPhone();
 
+        bonusProgram.clickButtonIssueCard();
+        bonusProgram.closeAlert();
+
+        Assertions.assertTrue(bonusProgram.displayH3WithCardHasBeenIssued(), "Карта бонуса не была оформлена");
     }
 
     @AfterEach
