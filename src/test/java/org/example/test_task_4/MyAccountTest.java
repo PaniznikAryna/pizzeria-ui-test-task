f97d4c030f2b0ae8d7874f714a3b5898fb9aed91
Paniznik Aryna <arinapaniznik4@gmail.com>
2025-10-13 12:09:55 +0300

    wrote tests for the My Account

--- src/test/java/org/example/test_task_4/MyAccountTest.java
+++ src/test/java/org/example/test_task_4/MyAccountTest.java
@@ -1,5 +1,7 @@
 package org.example.test_task_4;
 
+import org.example.pages.LoginPage;
+import org.example.pages.MyAccount;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,16 +11,26 @@ public class MyAccountTest {
     //- Загрузка файла +--
 
     WebDriver driver;
+    LoginPage loginPage;
+    MyAccount myAccount;
 
     @BeforeEach
     void setUp(){
         ChromeOptions chromeOptions = new ChromeOptions();
         driver = new ChromeDriver(chromeOptions);
         driver.get("https://pizzeria.skillbox.cc/my-account/");
+        loginPage = new LoginPage(driver);
+        myAccount = new MyAccount(driver);
     }
 
     @Test
     void testUploadingFile(){
+        loginPage.login("test1233314143", "testuser");
+
+        myAccount.clickButtonDataAccount();
+        driver.get("https://pizzeria.skillbox.cc/my-account/edit-account/");
+
+        myAccount.uploadFile();
 
     }
 
