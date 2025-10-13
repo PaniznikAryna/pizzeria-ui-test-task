93f04f4e8496a2bb6d46653c4aa172f4f33c1104
Paniznik Aryna <arinapaniznik4@gmail.com>
2025-10-13 12:10:14 +0300

    wrote tests for the My Account

--- src/test/java/org/example/test_task_4/MyAccountTest.java
+++ src/test/java/org/example/test_task_4/MyAccountTest.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class MyAccountTest {
-    //- Загрузка файла +--
 
     WebDriver driver;
     LoginPage loginPage;
