d5d0b7ea134c5feeb3a98269f9c3647cbdfc23d0
Paniznik Aryna <arinapaniznik4@gmail.com>
2025-10-14 11:21:07 +0300

    wrote tests for the Making an Order

--- src/main/java/org/example/pages/MakingAnOrder.java
+++ src/main/java/org/example/pages/MakingAnOrder.java
@@ -1,4 +1,122 @@
 package org.example.pages;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class MakingAnOrder {
+    private WebDriver driver;
+
+    public MakingAnOrder(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void goToMakingAnOrder() {
+        By basketLocator = By.xpath("//a[@href=\"http://pizzeria.skillbox.cc/checkout/\"]");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(basketLocator)).click();
+        } catch (StaleElementReferenceException e) {
+            wait.until(ExpectedConditions.elementToBeClickable(basketLocator)).click();
+        }
+    }
+
+    @FindBy(xpath = "//input[@type=\"date\"]")
+    private WebElement inputDate;
+
+    public void setDate(String date) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(inputDate));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].value = arguments[1];", inputDate, date);
+    }
+
+
+    public String getDate() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(inputDate));
+        return inputDate.getAttribute("value");
+    }
+
+    @FindBy(xpath = "//input[@id=\"billing_first_name\"]")
+    private WebElement inputFirstName;
+
+    @FindBy(xpath = "//input[@id=\"billing_last_name\"]")
+    private WebElement inputLastName;
+
+    @FindBy(xpath = "//input[@id=\"billing_address_1\"]")
+    private WebElement inputAddress;
+
+    @FindBy(xpath = "//input[@id=\"billing_city\"]")
+    private WebElement inputCity;
+
+    @FindBy(xpath = "//input[@id=\"billing_state\"]")
+    private WebElement inputState;
+
+    @FindBy(xpath = "//input[@id=\"billing_postcode\"]")
+    private WebElement inputPostcode;
+
+    @FindBy(xpath = "//input[@id=\"billing_phone\"]")
+    private WebElement inputPhone;
+
+    @FindBy(xpath = "//input[@id=\"billing_email\"]")
+    private WebElement inputEmail;
+
+    public void setForm() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        inputFirstName.sendKeys("test");
+        inputLastName.sendKeys("test");
+        inputAddress.sendKeys("test");
+        inputCity.sendKeys("test");
+        inputState.sendKeys("test");
+        inputPostcode.sendKeys("213342");
+        inputPhone.sendKeys("+79991231231");
+        inputEmail.clear();
+        inputEmail.sendKeys("test@exemple.com");
+        WebElement countryDropdown = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".select2-selection--single")));
+        countryDropdown.click();
+
+        WebElement searchInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.select2-search__field")));
+        searchInput.sendKeys("Belarus");
+
+        WebElement result = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[contains(@class,'select2-results__option') and text()='Belarus']")));
+        result.click();
+    }
+
+    @FindBy(xpath = "//input[@id='payment_method_cod']")
+    private WebElement radioPaymentOnDelivery;
+
+    public void selectPaymentOnDelivery() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(radioPaymentOnDelivery));
+        radioPaymentOnDelivery.click();
+    }
+
+    @FindBy(xpath = "//input[@id='terms']")
+    private WebElement checkboxTerms;
+
+    public void acceptTermsAndConditions() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(checkboxTerms));
+        if (!checkboxTerms.isSelected()) {
+            checkboxTerms.click();
+        }
+    }
+
+    @FindBy(xpath = "//button[@id=\"place_order\"]")
+    private WebElement buttonMakingAnOrder;
+
+    public void clickButtonMakingAnOrder(){
+        buttonMakingAnOrder.click();
+    }
+
+
 }
--- src/test/java/org/example/test_task_4/MakingAnOrderTest.java
+++ src/test/java/org/example/test_task_4/MakingAnOrderTest.java
@@ -1,30 +1,93 @@
 package org.example.test_task_4;
 
+import org.example.pages.BasketPage;
+import org.example.pages.LoginPage;
+import org.example.pages.MakingAnOrder;
+import org.example.pages.MenuPage;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.time.LocalDate;
 
 public class MakingAnOrderTest {
-    //- Установка даты заказа +--
-    //- Успешное оформление заказа с оплатой наличными +--
-
     WebDriver driver;
+    MakingAnOrder makingAnOrder;
+    LoginPage loginPage;
+    MenuPage menuPage;
+    BasketPage basket;
 
     @BeforeEach
     void setUp(){
         ChromeOptions chromeOptions = new ChromeOptions();
         driver = new ChromeDriver(chromeOptions);
         driver.get("https://pizzeria.skillbox.cc/checkout/");
+        makingAnOrder = new MakingAnOrder(driver);
+        loginPage = new LoginPage(driver);
+        menuPage = new MenuPage(driver);
+        basket = new BasketPage(driver);
     }
 
     @Test
-    void testSettingTheOrderDate(){
+    void testSettingTheOrderDate() throws InterruptedException {
+        driver.get("https://pizzeria.skillbox.cc/my-account/");
+        loginPage.login("test1233314143", "testuser");
 
+        loginPage.goToMenu();
+        menuPage.addDrinkToBasket();
+        Thread.sleep(1000);
+        menuPage.goToBasket();
+        basket = new BasketPage(driver);
+
+        makingAnOrder.goToMakingAnOrder();
+        makingAnOrder = new MakingAnOrder(driver);
+
+        LocalDate tomorrow = LocalDate.now().plusDays(1);
+        String expectedDate = tomorrow.toString();
+
+        makingAnOrder.setDate(expectedDate);
+
+        String actualDate = makingAnOrder.getDate();
+        Assertions.assertEquals(expectedDate, actualDate, "Дата в поле не соответствует ожидаемой");
     }
 
+
     @Test
-    void testMakingTheOrderWithCashPayment(){
+    void testMakingTheOrderWithCashPayment() throws InterruptedException {
+        driver.get("https://pizzeria.skillbox.cc/my-account/");
+        loginPage.login("test1233314143", "testuser");
+
+        loginPage.goToMenu();
+        menuPage.addDrinkToBasket();
+        Thread.sleep(1000);
+        menuPage.goToBasket();
+        basket = new BasketPage(driver);
+
+        makingAnOrder.goToMakingAnOrder();
+        makingAnOrder = new MakingAnOrder(driver);
+
+        makingAnOrder.setForm();
+        makingAnOrder.selectPaymentOnDelivery();
+        makingAnOrder.acceptTermsAndConditions();
+        makingAnOrder.clickButtonMakingAnOrder();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//p[contains(@class,'woocommerce-thankyou-order-received') and contains(text(),'Спасибо! Ваш заказ был получен.')]")
+        ));
+
+        WebElement paymentInfo = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//p[contains(text(),'Оплата наличными при доставке заказа.')]")
+        ));
+
+        Assertions.assertTrue(successMessage.isDisplayed(), "Сообщение о получении заказа не найдено");
+        Assertions.assertTrue(paymentInfo.isDisplayed(), "Информация об оплате наличными не найдена");
 
     }
 
