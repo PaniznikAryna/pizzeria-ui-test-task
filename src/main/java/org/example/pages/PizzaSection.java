6feb4f4593e3c1e522bb7d258e7b3a376050bef9
Paniznik Aryna <arinapaniznik4@gmail.com>
2025-10-14 15:29:32 +0300

    wrote tests for the Pizza Section

--- src/main/java/org/example/pages/PizzaSection.java
+++ src/main/java/org/example/pages/PizzaSection.java
@@ -1,4 +1,84 @@
 package org.example.pages;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PizzaSection {
+    private WebDriver driver;
+
+    public PizzaSection(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//select[@name='orderby']")
+    private WebElement sortDropdown;
+
+    @FindBy(xpath = "//button[text()='Применить']")
+    private WebElement filterButton;
+
+    @FindBy(xpath = "//ul[contains(@class,'products')]/li")
+    private List<WebElement> pizzaItems;
+
+    public void sortBy(String value) {
+        Select select = new Select(sortDropdown);
+        select.selectByValue(value);
+
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//ul[contains(@class,'products')]/li")));
+    }
+
+    public void filterByPrice(int leftOffset, int rightOffset) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(@class,'price_slider_wrapper')]")));
+
+        WebElement leftHandle = driver.findElement(By.xpath("(//span[contains(@class,'ui-slider-handle')])[1]"));
+        WebElement rightHandle = driver.findElement(By.xpath("(//span[contains(@class,'ui-slider-handle')])[2]"));
+
+        wait.until(ExpectedConditions.visibilityOf(leftHandle));
+        wait.until(ExpectedConditions.visibilityOf(rightHandle));
+
+        Actions actions = new Actions(driver);
+        actions.clickAndHold(leftHandle).moveByOffset(leftOffset, 0).release().perform();
+        actions.clickAndHold(rightHandle).moveByOffset(rightOffset, 0).release().perform();
+
+        wait.until(ExpectedConditions.elementToBeClickable(filterButton)).click();
+
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//ul[contains(@class,'products')]/li")));
+    }
+
+    public List<Integer> getPizzaPrices() {
+        return pizzaItems.stream().map(item -> Integer.parseInt(item.findElement(By.xpath(".//bdi")).getText().replaceAll("[^\\d]", "")) / 100).collect(Collectors.toList());
+    }
+
+    @FindBy(xpath = "//a[@data-product_id=\"425\"]")
+    private WebElement buttonInBasket;
+
+    public void clickButtonInBasket(){
+        buttonInBasket.click();
+    }
+
+    @FindBy(xpath = "//a[@title=\"Подробнее\"]")
+    private WebElement buttonMoreDetails;
+
+    public void clickButtonMoreDetails(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOf(buttonMoreDetails));
+        buttonMoreDetails.click();
+    }
+
+    @FindBy(xpath = "//td[@class='product-name']//a")
+    private WebElement itemName;
+
+    public String getItemName() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOf(itemName));
+        return itemName.getText();
+    }
 }
--- src/test/java/org/example/test_task_4/PizzaSectionTest.java
+++ src/test/java/org/example/test_task_4/PizzaSectionTest.java
@@ -1,44 +1,63 @@
 package org.example.test_task_4;
 
+import org.example.pages.PizzaSection;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.List;
+
 public class PizzaSectionTest {
-    //- Применение сортировки пицц +--
-    //- Фильтрация по цене +--
-    //- Добавление пиццы в корзину +--
 
     WebDriver driver;
+    PizzaSection pizzaSection;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         ChromeOptions chromeOptions = new ChromeOptions();
         driver = new ChromeDriver(chromeOptions);
         driver.get("https://pizzeria.skillbox.cc/product-category/menu/pizza/");
+        pizzaSection = new PizzaSection(driver);
     }
 
     @Test
-    void testPizzaSorting(){
+    void testPizzaSorting() {
+        pizzaSection.sortBy("price");
+        List<Integer> prices = pizzaSection.getPizzaPrices();
 
+        for (int i = 1; i < prices.size(); i++) {
+            Assertions.assertTrue(prices.get(i) >= prices.get(i - 1),
+                    "Цены не отсортированы по возрастанию: " + prices);
+        }
     }
 
     @Test
     void testFilteringByPrice(){
+        pizzaSection.filterByPrice(30, -40);
 
-    }
+        List<Integer> prices = pizzaSection.getPizzaPrices();
 
-    @Test
-    void testAddingPizzaToTheBasket(){
-
-    }
-
-    @AfterEach
-    void tearDown(){
-        if (driver != null){
-            driver.quit();
+        for (Integer price : prices) {
+            Assertions.assertTrue(price >= 440 && price <= 510,
+                    "Цена вне диапазона: " + price);
         }
     }
 
+    @Test
+    void testAddingPizzaToTheBasket() {
+        pizzaSection.clickButtonInBasket();
+        pizzaSection.clickButtonMoreDetails();
+
+        String currentPizzaName = pizzaSection.getItemName();
+
+        Assertions.assertEquals("Пицца \"4 в 1\"", currentPizzaName, "Пицца в корзине не соответствует ожидаемой Пиццы \"4 в 1\"");
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
